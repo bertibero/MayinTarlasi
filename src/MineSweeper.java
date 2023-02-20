0e07382b3ad29c246621cfa9109e6e0bde25cf0c
Beraat <69609063+bertibero@users.noreply.github.com>
2023-02-21 01:01:39 +0300

    Add files via upload

---
+++ src/MineSweeper.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+public class MineSweeper {
+    int row;
+    int column;
+    int numberOfMines;
+    String[][] gameField;
+    int[][] locationsOfMines;
+
+    public MineSweeper(int row, int column) {
+        this.row = row;
+        this.column = column;
+        this.numberOfMines = (row * column) / 4;
+        this.gameField = new String[row][column];
+        this.locationsOfMines =new int[row * column][2];
+        placeMines ();
+    }
+    public void placeMines(){
+        int mines = 0;
+
+        for (int i = 0; i < gameField.length; i++){
+            for (int j = 0; j < gameField[i].length; j++){
+                int random = (int) (Math.random() * 10);
+                if (random >= 7 && mines < numberOfMines){
+                    gameField[i][j] = "*";
+                    locationsOfMines[mines][0] = i;
+                    locationsOfMines[mines][1] = j;
+                    mines++;
+                }else{
+                    gameField[i][j] = "-";
+                }
+            }
+        }
+    }
+    public void run(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Mayınların lokasyonları :");
+        printGameTable(gameField);
+
+        String[][] runningGame = new String[row][column];
+        for (int i = 0; i < runningGame.length; i++){
+            for (int j = 0; j > runningGame.length; j++) {
+                runningGame[i][j] = "-";
+            }
+        }
+        int numberOfTry = 1;
+        while (numberOfTry <= row * column - numberOfMines){
+            System.out.println("==============");
+            System.out.println("Mayın Tarlasına Hoşgeldiniz ...!");
+            printGameTable(runningGame);
+            System.out.print("Satır sayısını giriniz :");
+            int rowNumber = input.nextInt() - 1;
+            System.out.print("Sütun sayısını giriniz :");
+            int columnNumber = input.nextInt() - 1;
+            if (rowNumber < 0 || rowNumber > runningGame.length - 1 || columnNumber < 0 || columnNumber > runningGame[0].length) continue;
+            numberOfTry++;
+            if (gameField[rowNumber][columnNumber].equals("*")){
+                System.out.println("Oyunu Kaybettiniz !");
+                return;
+            }else {
+                int numberOfNearMines=0;
+                if (rowNumber > 0 && columnNumber > 0 && gameField[rowNumber - 1][columnNumber - 1].equals("*")){
+                    numberOfNearMines++;
+                }
+                if (rowNumber > 0 && gameField[rowNumber - 1][columnNumber].equals("*")){
+                    numberOfNearMines++;
+                }
+                if (rowNumber > 0 && columnNumber < gameField[0].length - 1&&gameField[rowNumber - 1][columnNumber + 1].equals("*")){
+                    numberOfNearMines++;
+                }
+                if (columnNumber > 0 && gameField[rowNumber][columnNumber - 1].equals("*")){
+                    numberOfNearMines++;
+                }
+                if (columnNumber < gameField[0].length - 1 && gameField[rowNumber][columnNumber + 1].equals("*")){
+                    numberOfNearMines++;
+                }
+                if (rowNumber < gameField.length - 1 && columnNumber > 0 && gameField[rowNumber + 1][columnNumber - 1].equals("*")){
+                    numberOfNearMines++;
+                }
+                if (rowNumber < gameField.length - 1 &&gameField[rowNumber + 1][columnNumber].equals("*")){
+                    numberOfNearMines++;
+                }
+                if (rowNumber < gameField.length - 1 && columnNumber < gameField[0].length - 1 && gameField[rowNumber + 1][columnNumber + 1].equals("*")){
+                    numberOfNearMines++;
+                }
+                runningGame[rowNumber][columnNumber]=" " + numberOfNearMines;
+            }
+            System.out.println("Tebrikler ! Oyunu kazandınız.");
+            printGameTable(runningGame);
+        }
+    }
+    void printGameTable(String[][] game){
+        for(int i = 0; i < game.length; i++){
+            for (int j = 0; j < game[i].length; j++){
+                System.out.print(game[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
